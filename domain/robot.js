4a29d62ef3233fc226239ad3bb5def2e3d08bb1e
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2019-12-18 13:31:10 +0000

    Adding test case to cover robot move and change of orientation

--- domain/robot.js
+++ domain/robot.js
@@ -3,5 +3,27 @@ module.exports = class Robot {
         this.x = x;
         this.y = y;
         this.orientation = orientation;
+        this.lost = false;
+    }
+
+    setOrientation(newOrientation) {
+        this.orientation = newOrientation;
+    }
+
+    move() {
+        switch (this.orientation) {
+            case 'N':
+                this.y = this.y + 1;
+                break;
+            case 'S':
+                this.y = this.y - 1;
+                break;
+            case 'E':
+                this.x = this.x + 1;
+                break;
+            case 'W':
+                this.x = this.x - 1;
+                break;
+        }
     }
 }
--- tests/robot.test.js
+++ tests/robot.test.js
@@ -1,14 +1,44 @@
 const Robot = require('../domain/robot');
-
+let robot = null;
 
 describe('robot', () => {
 
-    it('should properly initialize a robot object', () => {
-        const robot = new Robot(1, 1, 'N');
+    beforeEach(() => {
+        robot = new Robot(1, 1, 'N');
+    });
 
+    it('should properly initialize a robot object', () => {
         expect(robot.orientation).toBe('N');
         expect(robot.x).toBe(1);
         expect(robot.y).toBe(1);
+        expect(robot.lost).toBe(false);
+    });
+
+    it('should reset the current orientation', () => {
+
+        robot.setOrientation('S');
+
+        expect(robot.orientation).toBe('S');
+    });
+
+    describe('move', () => {
+        it('should move the robot one point in current orientation', () => {
+            robot.move();
+            expect(robot.y).toBe(2);
+            robot.move();
+            expect(robot.y).toBe(3);
+        });
+
+        it('should move the robot one point in the new direction', () => {
+            robot.move();
+            expect(robot.y).toBe(2);
+
+            robot.setOrientation('E');
+            robot.move();
+            
+            expect(robot.x).toBe(2);
+            expect(robot.y).toBe(2);
+        });
     });
 
 });
