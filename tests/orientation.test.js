836952e0893b607850c93838613d83df53d86c89
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2019-12-18 13:07:02 +0000

    Adding test cases to check if command is a valid request for change in orientation

--- tests/orientation.test.js
+++ tests/orientation.test.js
@@ -19,5 +19,19 @@ describe('change', () => {
         newOrientation = orientation.change(newOrientation, commands.left);
         expect(newOrientation).toBe('E');
 
-;    });
+        newOrientation = orientation.change(newOrientation, commands.left);
+        expect(newOrientation).toBe('N');
+
+        newOrientation = orientation.change(newOrientation, commands.left);
+        expect(newOrientation).toBe('W');
+
+  });
+});
+
+describe('isOrientationCommand', () => {
+    it('should validate if the command is releated to orientation change request', () => {
+        expect(orientation.isOrientationCommand('L')).toBe(true);
+        expect(orientation.isOrientationCommand('R')).toBe(true);
+        expect(orientation.isOrientationCommand('K')).toBe(false);
+    })
 });
